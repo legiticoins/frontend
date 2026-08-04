d26adf3ec3cd3f05060b794ab1fcd39ef3abaa3e
Omri H <omrih.e@protonmail.com>
2026-08-04 17:39:08 +0300

    feat: auth, remove vibecoding shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
-import svelte from '@astrojs/svelte';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react(), svelte(), icon({iconDir: 'src/assets/icons'})]
+  integrations: [react(), svelte(), icon({ iconDir: "src/assets/icons" })],
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  output: "server",
+
+  server: {
+    host: "0.0.0.0",
+  },
+
+  env: {
+    schema: {
+      SITE_ROOT: envField.string({
+        context: "client",
+        access: "public",
+        default: "https://wwlc.legiti.dev",
+      }),
+      BACKEND_URL: envField.string({
+        context: "client",
+        access: "public",
+        default: "https://api.wwlc.legiti.dev",
+      }),
+      MCAUTH_CLIENT_ID: envField.string({
+        context: "server",
+        access: "public",
+      }),
+      MCAUTH_CLIENT_SECRET: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
 });
---
+++ src/pages/login.ts
@@ -0,0 +1,83 @@
+import type { APIRoute, AstroCookies } from "astro";
+import { MCAUTH_CLIENT_ID, MCAUTH_CLIENT_SECRET } from "astro:env/server";
+import { BACKEND_URL, SITE_ROOT } from "astro:env/client";
+import { randomBytes, createHash } from "crypto";
+
+const REDIRECT_URI = `${SITE_ROOT}/login`;
+
+export async function isLoggedIn(cookies: AstroCookies) {
+  return (
+    cookies.get("authorization.sessionToken") && cookies.get("profile.uuid")
+  );
+}
+
+export const GET: APIRoute = async ({ url, cookies, redirect, request }) => {
+  const loggedIn = await isLoggedIn(cookies);
+  if (loggedIn) {
+    return redirect(cookies.get("oauth.redirect")?.value ?? "/", 302);
+  }
+
+  const RAW_SEARCH_PARAMS = url.href.split("?")[1];
+  const SEARCH_PARAMS = new URLSearchParams(RAW_SEARCH_PARAMS);
+
+  // Client hasnt logged in yet
+  if (!SEARCH_PARAMS.get("code")) {
+    const state = randomBytes(32).toString("hex");
+
+    cookies.set("oauth.state", state, {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 600,
+    });
+
+    const redirectTo = request.headers.get("referer") ?? "/";
+    cookies.set("oauth.redirect", redirectTo, {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 600,
+    });
+    const authUrl = new URL("https://mc-auth.com/oAuth2/authorize");
+    authUrl.searchParams.append("client_id", MCAUTH_CLIENT_ID);
+    authUrl.searchParams.append("redirect_uri", REDIRECT_URI);
+    authUrl.searchParams.append("scope", "profile");
+    authUrl.searchParams.append("response_type", "code");
+    authUrl.searchParams.append("state", state);
+    return redirect(authUrl.toString(), 302);
+  }
+
+  // Client has redirected. We got the code.
+  const mcAuthPostRequestBody = {
+    client_id: MCAUTH_CLIENT_ID,
+    client_secret: MCAUTH_CLIENT_SECRET,
+    code: SEARCH_PARAMS.get("code"),
+    redirect_uri: REDIRECT_URI,
+    grant_type: "authorization_code",
+  };
+
+  const backendResponse = await fetch(`${BACKEND_URL}/user/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(mcAuthPostRequestBody),
+  });
+  const backendResponseBody = await backendResponse.json();
+
+  if (
+    backendResponseBody.success == false ||
+    backendResponseBody.state != cookies.get("oauth.state")?.value
+  ) {
+    return redirect(`/login-fail`, 302);
+  }
+
+  // don't make the user confused on why they didnt log in cuz why not
+  if (!backendResponse.ok) return redirect(`/login-fail`, 302);
+
+  const { token, uuid } = backendResponseBody;
+
+  cookies.set("authorization.token", token, { path: "/" });
+  cookies.set("profile.uuid", uuid, { path: "/" });
+
+  const redirectBack = cookies.get("oauth.redirect")?.value ?? "/";
+  cookies.delete("oauth.redirect");
+
+  return redirect(redirectBack, 302);
+};

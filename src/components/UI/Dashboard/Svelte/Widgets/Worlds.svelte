73839e4c5e88530bfd0a7eb590bbe664ef88422a
barden <theofficialbarden@gmail.com>
2026-08-05 20:56:35 +0530

    APPROVED AND UNAPPROVED WORLDS oops all caps

--- src/components/UI/Dashboard/Svelte/Widgets/Worlds.svelte
+++ src/components/UI/Dashboard/Svelte/Widgets/Worlds.svelte
@@ -1,14 +1,14 @@
 <script>
      const { 
-     worldTitle = "My World", 
-     // worldIcon = "cobblestone", 
-     // worldDesc = "This is a World Description" 
+          world
      } = $props();
+
+     
 
 </script>
 
 <main class="hover:cursor-pointer group aspect-square relative md:max-w-100 max-h-45 md:max-h-50 duration-300 w-full h-full border rounded-ui shrink-0 overflow-hidden">
      <div class="bottom-0 absolute p-2 duration-300 w-full group-hover:opacity-100 opacity-0">
-          <h1>{worldTitle}</h1>
+          <h1>{world.name}</h1>
      </div>
-</main>
+</main>   
--- src/components/UI/Dashboard/Svelte/Worlds.svelte
+++ src/components/UI/Dashboard/Svelte/Worlds.svelte
@@ -1,33 +1,62 @@
 <script lang=ts>
+  import { onMount } from "svelte";
+
 
      // Widgets
      import WorldWidget from "./Widgets/Worlds.svelte"
      
      const { data } = $props();
 
-     interface IWorldData {
-          uuid: string,
-          name: string
-     }
-     const worlds: IWorldData[] = $state([])
+     const approvedWorlds= $state<any[]>([])
+     const unapprovedWorlds= $state<any[]>([])
      
-     $inspect(data)
+     const apiString = `https://api.legiti.dev/owner/${data.userUUID}`
+     let worldsData= $state<any[]>([]);
+     
+     onMount( async () => {
+          // GET ALL WORLDS FROM API.LEGITI.DEV
+          worldsData = await fetch(apiString, {
+               headers: {
+                    'Access-Control-Allow-Origin': 'http://localhost:3000'
+               }
+          }).then(async response => await response.json())
+          
+          // Sort by Approved / UnApproved.
+          worldsData.forEach(async (world: any) => {
+               const response = await fetch(`https://wwlc.legiti.dev/api/world/${world.world_uuid}`).then(async response => response.json())
+               if (response.success == "false") {
+                    unapprovedWorlds.push(world)
+               } else {
+                    approvedWorlds.push(world)
+               }
+          })
+
+     })
 
 </script>
 
 <div class="w-full md:px-5 font-ui">
      <h1 class="text-mobile-title mb-5">My Worlds</h1>
+
+     <p class="my-5">Your worlds must first be approved by the admins at Legitidevs to start creating jobs.</p>
+
      <!-- WORLDS Div -->
-     <div class="max-h-150 h-full w-full flex flex-wrap content-start gap-2">
+     <div class="max-h-150 h-full w-full flex flex-col gap-2">
 
           <!-- If No Worlds -->
-          {#if worlds.length === 0}
-               <p class="opacity-40">You have no worlds!</p>
+          {#if worldsData.length === 0}
+               <p class="opacity-40">Loading..</p>
           {/if}
 
-          {#each worlds as world}
-               <WorldWidget />
+          <p>Approved Worlds</p>
+          {#each approvedWorlds as world}
+               <WorldWidget world={world} />
           {/each}
-          
+
+          <p>UnApproved Worlds</p>
+          {#each unapprovedWorlds as world}
+               <WorldWidget world={world} />
+          {/each}
+
      </div>
 </div>

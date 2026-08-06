695e374a2959efbc6a4bafa6664db28dce00eef5
barden <theofficialbarden@gmail.com>
2026-08-06 18:35:45 +0530

    Remove World Placeholder Image - It's ugly.

--- src/components/UI/Dashboard/Svelte/Widgets/Worlds.svelte
+++ src/components/UI/Dashboard/Svelte/Widgets/Worlds.svelte
@@ -3,10 +3,13 @@
 <script>
      const { world, image } = $props();
 
+     // World Icons HREF:
+     // const string = `https://raw.githubusercontent.com/jacobsjo/mcicons/refs/heads/icons/item/`
+     
 </script>
 
 <main class="hover:cursor-pointer group aspect-square relative md:max-w-100 max-h-45 md:max-h-50 duration-300 w-full h-full border rounded-ui shrink-0 overflow-hidden">
-     <img src={image} alt="placeholder">
+     <!-- <img src={string + world.icon + ".png"} alt="placeholder"> -->
      <div class="bottom-0 absolute p-2 duration-300 w-full group-hover:opacity-100 opacity-100 md:opacity-0 dark:bg-black/95 light:bg-white/99">
           <h1>[{world.version}] {world.name}</h1>
      </div>
